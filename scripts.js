16be361bf90e05ba7990af2f8fd197fae46c41e8
Ray <hjc440@nyu.edu>
2016-08-15 20:53:21 -0400

    final version of the web

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$("#pl1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp1").offset().top -290}, 2000);
+});
+
+$("#pl2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp2").offset().top -290}, 2000);
+});
+
+$("#pl3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp3").offset().top -290}, 2000);
+});
+
+$("#pl4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp4").offset().top -290}, 2000);
+});
+
+$("#pl5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp5").offset().top -290}, 2000);
+});
+
+$("#pl6").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#pp6").offset().top -290}, 2000);
+});
